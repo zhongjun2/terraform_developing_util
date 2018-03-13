eaf6269fa733ae2f5afa0d8c0b92f67a934faa81
zengchen1024 <chenzeng2@huawei.com>
2018-03-13 16:02:27 +0800

    add util for comparing sdk files between providers

--- utils/common.sh
+++ utils/common.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
-pre_treat_old_file () {
+pre_treat_old_file() {
     local f=$2
 
     if [ $1 = "rm" ]; then
-	rm ${f}*
-	echo $f
+        rm ${f}*
+        echo $f
     else
+        test ! -f $f && echo $f && return 0
         local fl=$(ls -l ${f}* | awk '{print $NF}' | sort | tail -n 1)
         echo ${fl}1
     fi
 }
 
-capitalize () {
+capitalize() {
     local w=$1
     local c=$(echo ${w:0:1})
     echo $(echo $c | tr [a-z] [A-Z])$(echo ${w:1})
 }
 
+has_prefix() {
+    local r=$(echo $1 | egrep "^$2")
+    test -n "$r" && echo "yes" || echo "no"
+}
+
 get_file_absolute_path() {
-	local cur_dir=$(pwd)
-	cd $(dirname $1)
-	echo "$(pwd)/$(basename $1)"
-	cd $cur_dir
+    local cur_dir=$(pwd)
+    cd $(dirname $1)
+    echo "$(pwd)/$(basename $1)"
+    cd $cur_dir
+}
+
+fmt_go_file() {
+    local f=$1
+    goimports -w $f
+    gofmt -w $f
 }
---
+++ utils/terraform_compare_provider_sdk_file.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+	echo "usage: $0 file dest_cloud_name [dest file]" $#
+	exit 1
+fi
+
+cur_dir=$(pwd)
+cd $(dirname $1)
+src=$(pwd)/$(basename $1)
+cd $cur_dir
+
+get_config="$(dirname $(which $0))/../config/get_config.sh"
+
+src_cloud_alias=$($get_config guess_cloud_alias $(dirname $src))
+test $? -ne 0 && echo "$1 is not a file belonging to a cloud" && exit 1
+src_cloud=$($get_config name $src_cloud_alias)
+
+dest_cloud_alias=$2
+dest_cloud=$($get_config name $dest_cloud_alias)
+test $? -ne 0 && echo "can not find cloud name: $dest_cloud_alias" && exit 1
+
+src_base_dir="$($get_config using_sdk $src_cloud_alias)/"
+src_relative_dir=${src#$src_base_dir}
+dest=$($get_config using_sdk $dest_cloud_alias)/$src_relative_dir
+if [ $# -eq 3 ]; then
+	dest=$($get_config using_sdk $dest_cloud_alias)/$3
+fi
+
+test ! -f $dest && echo "$dest is not exist" && exit 1
+
+echo $src
+echo $dest
+vimdiff $src $dest
